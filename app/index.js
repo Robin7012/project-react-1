825274b0584d3e8eebd788c5445ba59dcf8c5433
郑耿升 <398374199@qq.com>
2017-05-02 21:31:29 +0800

    使用webpack-dev-server

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ function component() {
     var element = $('<div></div>');
     /* lodash is required for the next line to work */
     // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.html(_.join(['Hi', 'webpack'], ' '));
+    element.html(_.join(['Hello', 'webpack'], ' '));
 
     // return element;
     return element.get(0);
--- dist/bundle.js
+++ dist/bundle.js
@@ -27446,7 +27446,7 @@ function component() {
     var element = __WEBPACK_IMPORTED_MODULE_1_jquery___default()('<div></div>');
     /* lodash is required for the next line to work */
     // element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.html(__WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['Hi', 'webpack'], ' '));
+    element.html(__WEBPACK_IMPORTED_MODULE_0_lodash___default.a.join(['Hello', 'webpack'], ' '));
 
     // return element;
     return element.get(0);

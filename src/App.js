ce62dc65901594acd30a3c8094e02595cfb7f75f
郑耿升 <398374199@qq.com>
2017-05-08 12:18:03 +0800

    新增todo

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import 'normalize.css'
 import './reset.css'
 import './App.css'
 import TodoInput from './TodoInput'
+import TodoItem from './TodoItem'
+
 
 class App extends Component {
   constructor(props){
@@ -10,22 +12,23 @@ class App extends Component {
     this.state = {
       newTodo:'',
       todoList:[
-        {id:'1',title:'第一个待办',status:'completed',deleted:false },
-        {id:'2',title:'第二个待办',status:'completed',deleted:false },
-        {id:'3',title:'第三个待办',status:'completed',deleted:true }
+        // {id:'1',title:'第一个待办',status:'completed',deleted:false },
+        // {id:'2',title:'第二个待办',status:'completed',deleted:false },
+        // {id:'3',title:'第三个待办',status:'completed',deleted:true }
       ]
     }
   }
   render() {
     let todos  = this.state.todoList.map((item,index)=>{
-      return <li>{item.title}</li>
+      return <li key={index}>
+                <TodoItem todo={item} />
+             </li>
     })
-
     return (
       <div className="App">
         <h1>我的代办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo}/>
+          <TodoInput content={this.state.newTodo} onSubmit={this.addTodo.bind(this)}/>
         </div>
      
         <ol>
@@ -34,9 +37,24 @@ class App extends Component {
        </div>
     );
   }
-   addTodo(){
-     console.log('我得添加一个 todo 了')
+   addTodo(event){
+     this.state.todoList.push({
+       id: idMaker(),
+       title:event.target.value,
+       status:null,
+       delete:false
+     })
+      this.setState({
+        newTodo:'',
+        todoList:this.state.todoList
+     })
    }
 }
 
 export default App;
+
+let id = 0;
+function idMaker(){
+  id += 1;
+  return id;
+}

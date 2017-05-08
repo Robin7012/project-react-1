be900025a030d05e0d3ad98fd38287a5150f8144
郑耿升 <398374199@qq.com>
2017-05-08 10:46:57 +0800

    监听回车事件

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
       <div className="App">
         <h1>我的代办</h1>
         <div className="inputWrapper">
-          <TodoInput content={this.state.newTodo}/>
+          <TodoInput content={this.state.newTodo} />
         </div>
      
         <ol>

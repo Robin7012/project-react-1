124743dc786ab25ebe4cba20201a316c20647640
郑耿升 <398374199@qq.com>
2017-05-08 09:34:37 +0800

    react 生命周期

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,8 +1,19 @@
 import React from 'react';
-class Welcome extends React.Component{
-    render(){
-        return <h1>Hello, {this.props.name}</h1>;
+class Welcome extends React.Component {
+    constructor() {
+        super();
+        console.log('我已经在 constructor 里将 props 和 state 初始化好了');
+    }
+    componentWillMount() {
+        console.log('运行到这里的话，说明马上就要运行 render 了')
+    }
+    render() {
+        console.log('嗯，这里是 render');
+        return <h1>React 生命周期</h1>;
+    }
+    componentDidMount() {
+        console.log('已经挂载到页面里了')
     }
 }
 
-export default Welcome 
+export default Welcome
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import './index.css';
 import Welcome from './Welcome'
 
-function tick() {
+/*function tick() {
   const element = (
     <div>
       <h1>Hello,world</h1>
@@ -12,6 +12,36 @@ function tick() {
     </div>
   )
   ReactDOM.render(element, document.getElementById('root'))
+}*/
+class Clock extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {date : new Date()};
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+ componentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+    tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello,world</h1>
+        <h2>It is  {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    )
+  }
 }
-// setInterval(tick, 1000);
-ReactDOM.render(<Welcome name="frank"/>, document.getElementById('root'))
+// setInterval(tick, 1000); 
+ReactDOM.render(<Welcome />,
+          document.getElementById('root'))

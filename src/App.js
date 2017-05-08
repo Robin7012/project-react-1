eb9bb0574cbe7ad053b45eedacfc87fc0c2f6e99
郑耿升 <398374199@qq.com>
2017-05-08 14:40:50 +0800

    更新 TodoItem TodoInput样式

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ class App extends Component {
           />
         </div>
      
-        <ol>
+        <ol className="todoList">
           {todos}
         </ol>
        </div>
--- src/TodoInput.js
+++ src/TodoInput.js
@@ -1,8 +1,11 @@
 import React,{ Component } from 'react';
+import './TodoInput.css'
 
 export default class TodoInput extends Component{
     render(){
-        return <input type="text" value={this.props.content} onKeyPress={this.submit.bind(this)}
+        return <input type="text" value={this.props.content} 
+        className="TodoInput"
+        onKeyPress={this.submit.bind(this)}
         onChange = {this.changeTitle.bind(this)}
         />
     }
